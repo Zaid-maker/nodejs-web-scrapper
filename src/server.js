517d1741c74891e05fb6b9024e9bf79231a9947b
DevMirza <pzhafeez@gmail.com>
2023-09-25 08:39:10 +0000

    uhh

--- src/server.js
+++ src/server.js
@@ -1,18 +1,18 @@
 // src/server.js
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const bodyParser = require('body-parser');
-const scrapeProducts = require('./scraper');
+const bodyParser = require("body-parser");
+const scrapeProducts = require("./scraper");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // middleware for routes
-app.use('/', scrapeProducts);
+app.use("/", scrapeProducts);
 
 // Server listening
-server = app.listen(3000, () => {
+server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
